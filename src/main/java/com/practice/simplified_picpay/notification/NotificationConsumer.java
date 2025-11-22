bb97cb7cc5cb3a39aa9b5b1e14b7c992f2a3f393
Hugo Angelis <hugo11angelis@hotmail.com>
2025-11-22 15:09:24 -0300

    feat: implement notifications with kafka

---
+++ src/main/java/com/practice/simplified_picpay/notification/NotificationConsumer.java
@@ -0,0 +1,30 @@
+package com.practice.simplified_picpay.notification;
+
+
+import com.practice.simplified_picpay.transaction.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class NotificationConsumer {
+    private RestClient restClient;
+
+    public NotificationConsumer(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc")
+                .build();
+    }
+
+
+    // Idealmente seria melhor ter um objeto notificação com a transação dentro com dtos na aplicação
+    @KafkaListener(topics = "transaction-notification", groupId = "picpay-desafio-backend")
+    public void receiveNotification(Transaction transaction) {
+        var response = restClient.get()
+                .retrieve()
+                .toEntity(Notification.class);
+
+        if(response.getStatusCode().isError() || !response.getBody().message())
+            throw new NotificationException("Error sending notification");
+    }
+}
---
+++ src/main/java/com/practice/simplified_picpay/notification/NotificationException.java
@@ -0,0 +1,8 @@
+package com.practice.simplified_picpay.notification;
+
+public class NotificationException extends RuntimeException {
+
+    public NotificationException(String message) {
+        super(message);
+    }
+}
